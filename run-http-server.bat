99023d0ce02b623e4cb0fb8069e0dcdfdb65b6a4
Inês Costa <102389923+Finicky-Cat@users.noreply.github.com>
2023-06-25 22:27:59 +0100

    Update run-http-server.bat

--- run-http-server.bat
+++ run-http-server.bat
@@ -1,7 +1,7 @@
-REM set the class path,
-REM assumes the build was executed with maven copy-dependencies
+REM Set the classpath
+REM Assumes the build was executed with Maven copy-dependencies
 
-SET ECOURSE_CP=ecourse.app.sharedboard.http.server/target/http.server-1.0.0.jar:ecourse.app.sharedboard.http.server/target/dependency/*;
+SET ECOURSE_CP=ecourse.app.sharedboard.http.server/target/http.server-1.0.0.jar;ecourse.app.sharedboard.http.server/target/dependency/*;
 
-REM call the java VM, e.g,
-java -cp %ECOURSE_CP% eapli.ecourse.app.sharedBoard.HttpSharedBoardServerApp %1
+REM Call the Java VM, e.g.,
+java -cp "%ECOURSE_CP%" eapli.ecourse.app.sharedBoard.HttpSharedBoardServerApp %1
